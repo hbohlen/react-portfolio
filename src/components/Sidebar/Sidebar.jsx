822a9e0ec2194b7cf316d70d3f7786338c812f52
hbohlen <bohlenhayden@gmail.com>
2022-04-17 13:31:02 -0500

    created sidebar component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,13 +2,15 @@ import { Link } from 'react-router-dom'
 import './sidebar.scss'
 
 import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
 
 const Sidebar = () => {
   return (
     <div className="nav-bar">
+      Hello
       <Link className="logo" to="/">
         <img src={LogoS} alt="logo" />
-        <img src={LogoS} alt="logo" />
+        <img src={LogoSubtitle} alt="logo" />
       </Link>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
